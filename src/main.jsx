5e63a8963fa58814c951c7ad9a5c6d230e36d4a1
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2026-08-13 21:20:20 -0300

    Criação de estilo global

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
 import { Login } from './containers/Login'
+import GlobalStyles from './styles/globalStyles'
+
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Login/>
+    <GlobalStyles/>
   </StrictMode>,
 )
---
+++ src/styles/globalStyles.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        outline: none;
+    }
+
+    button{
+
+        cursor: pointer;
+    }
+
+`
+export default GlobalStyles
